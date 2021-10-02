e501cec66d113b12ae8b4b58e61e90c8f778b285
Antony Okilipa <aokilipa@gmail.com>
2021-10-02 09:58:21 +0300

    initial commit

---
+++ app/src/main/java/io/github/payoneer/MainActivity.java
@@ -0,0 +1,21 @@
+package io.github.payoneer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.payoneer.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
